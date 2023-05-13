daa39a79f5eea66ca36680b3bb11639300083ed2
Bespalov Artem <75415627+BespalovArtem@users.noreply.github.com>
2023-05-13 12:05:08 +0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,104 +1,104 @@
 #include "sort.h"
 
-void udtMergeSort(Udt *udt)
+void UdtMergeSort(Udt *udt)
 {
-	const int size = udtSize(udt);
+	const int size = UdtSize(udt);
 	Udt left, right;
 	
 	if (size <= 1)
 		return;
 	
-	udtCreate(&left, size);
-	udtCreate(&right, size);
+	UdtCreate(&left, size);
+	UdtCreate(&right, size);
 	
-	udtPartition(udt, &left, &right);
-	udtMergeSort(&left);
-	udtMergeSort(&right);
-	udtMerge(udt, &left, &right);
+	UdtPartition(udt, &left, &right);
+	UdtMergeSort(&left);
+	UdtMergeSort(&right);
+	UdtMerge(udt, &left, &right);
 
-	udtDestroy(&left);
-	udtDestroy(&right);
+	UdtDestroy(&left);
+	UdtDestroy(&right);
 }
 
-void udtPartition(Udt *udt, Udt *udt1, Udt *udt2)
+void UdtPartition(Udt *udt, Udt *udt1, Udt *udt2)
 {
-	const int size = udtSize(udt);
+	const int size = UdtSize(udt);
 	int i;
 	Udt tmp;
 	UDT_TYPE cur;
 	
-	udtCreate(&tmp,	size);
+	UdtCreate(&tmp,	size);
 	
-	while (!udtEmpty(udt))
+	while (!UdtEmpty(udt))
 	{
-		udtPush(&tmp, udtTop(udt));
-		udtPop(udt);
+		UdtPush(&tmp, UdtTop(udt));
+		UdtPop(udt);
 	}
 	
 	for (i = 0; i < size / 2; i++)
 	{
-		cur = udtTop(&tmp);
+		cur = UdtTop(&tmp);
 		
-		udtPush(udt1, cur);
-		udtPop(&tmp);
+		UdtPush(udt1, cur);
+		UdtPop(&tmp);
 	}
 	
 	for (; i < size; i++)
 	{
-		cur = udtTop(&tmp);
+		cur = UdtTop(&tmp);
 		
-		udtPush(udt2, cur);
-		udtPop(&tmp);
+		UdtPush(udt2, cur);
+		UdtPop(&tmp);
 	}
 	
-	udtDestroy(&tmp);
+	UdtDestroy(&tmp);
 }
 
-void udtMerge(Udt *udt, Udt *udt1, Udt *udt2)
+void UdtMerge(Udt *udt, Udt *udt1, Udt *udt2)
 {
 	Udt tmp1, tmp2;
 	
-	udtCreate(&tmp1, udtSize(udt1));
-	udtCreate(&tmp2, udtSize(udt2));
+	UdtCreate(&tmp1, UdtSize(udt1));
+	UdtCreate(&tmp2, UdtSize(udt2));
 	
-	while (!udtEmpty(udt1))
+	while (!UdtEmpty(udt1))
 	{
-		udtPush(&tmp1, udtTop(udt1));
-		udtPop(udt1);
+		UdtPush(&tmp1, UdtTop(udt1));
+		UdtPop(udt1);
 	}
 
-	while (!udtEmpty(udt2))
+	while (!UdtEmpty(udt2))
 	{
-		udtPush(&tmp2, udtTop(udt2));
-		udtPop(udt2);
+		UdtPush(&tmp2, UdtTop(udt2));
+		UdtPop(udt2);
 	}
 	
-	while (!udtEmpty(&tmp1) && !udtEmpty(&tmp2))
+	while (!UdtEmpty(&tmp1) && !UdtEmpty(&tmp2))
 	{
-		if (udtTop(&tmp1)._key <= udtTop(&tmp2)._key)
+		if (UdtTop(&tmp1)._key <= UdtTop(&tmp2)._key)
 		{
-			udtPush(udt, udtTop(&tmp1));
-			udtPop(&tmp1);
+			UdtPush(udt, UdtTop(&tmp1));
+			UdtPop(&tmp1);
 		}
 		else
 		{
-			udtPush(udt, udtTop(&tmp2));
-			udtPop(&tmp2);
+			UdtPush(udt, UdtTop(&tmp2));
+			UdtPop(&tmp2);
 		}
 	}
 	
-	while (!udtEmpty(&tmp1))
+	while (!UdtEmpty(&tmp1))
 	{
-		udtPush(udt, udtTop(&tmp1));
-		udtPop(&tmp1);
+		UdtPush(udt, UdtTop(&tmp1));
+		UdtPop(&tmp1);
 	}
 	
-	while (!udtEmpty(&tmp2))
+	while (!UdtEmpty(&tmp2))
 	{
-		udtPush(udt, udtTop(&tmp2));
-		udtPop(&tmp2);
+		UdtPush(udt, UdtTop(&tmp2));
+		UdtPop(&tmp2);
 	}
 	
-	udtDestroy(&tmp1);
-	udtDestroy(&tmp2);
+	UdtDestroy(&tmp1);
+	UdtDestroy(&tmp2);
 }
