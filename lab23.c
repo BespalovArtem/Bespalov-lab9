b302054073cccfa5948c5d8c4badac8f9c54ef08
Bespalov Artem <75415627+BespalovArtem@users.noreply.github.com>
2023-05-13 11:48:24 +0300

    Update lab23.c

--- lab23.c
+++ lab23.c
@@ -7,7 +7,7 @@ typedef struct Node {
     struct Node *right;
 } Node;
 
-Node* createNode(int value) {
+Node* СreateNode(int value) {
     Node* node = (Node*) malloc(sizeof(Node));
     node->value = value;
     node->left = NULL;
@@ -15,7 +15,7 @@ Node* createNode(int value) {
     return node;
 }
 
-void insert(Node** root, int parentValue, int value, char child) {
+void Insert(Node** root, int parentValue, int value, char child) {
     if (*root == NULL) {
         return;
     }
@@ -27,28 +27,28 @@ void insert(Node** root, int parentValue, int value, char child) {
         }
         return;
     }
-    insert(&((*root)->left), parentValue, value, child);
-    insert(&((*root)->right), parentValue, value, child);
+    Insert(&((*root)->left), parentValue, value, child);
+    Insert(&((*root)->right), parentValue, value, child);
 }
 
-int getMaxDepth(Node* root) {
+int GetMaxDepth(Node* root) {
     if (root == NULL) {
         return 0;
     }
-    int leftDepth = getMaxDepth(root->left);
-    int rightDepth = getMaxDepth(root->right);
+    int leftDepth = GetMaxDepth(root->left);
+    int rightDepth = GetMaxDepth(root->right);
     return 1 + ((leftDepth > rightDepth) ? leftDepth : rightDepth);
 }
 
-int findMaxValue(Node* root, int maxDepth) {
+int FindMaxValue(Node* root, int maxDepth) {
     if (root == NULL) {
         return -1; 
     }
-    if (getMaxDepth(root) == maxDepth) {
+    if (GetMaxDepth(root) == maxDepth) {
         return root->value;
     }
-    int leftValue = findMaxValue(root->left, maxDepth);
-    int rightValue = findMaxValue(root->right, maxDepth);
+    int leftValue = FindMaxValue(root->left, maxDepth);
+    int rightValue = FindMaxValue(root->right, maxDepth);
     if (leftValue == -1 && rightValue == -1) {
         return -2; 
     } else if (leftValue == -1) {
@@ -59,16 +59,16 @@ int findMaxValue(Node* root, int maxDepth) {
         return (leftValue > rightValue) ? leftValue : rightValue; 
 }
 
-void printTree(Node* root, int level) {
+void PrintTree(Node* root, int level) {
     if (root == NULL) {
         return;
     }
-    printTree(root->right, level + 1);
+    PrintTree(root->right, level + 1);
     for (int i = 0; i < level; i++) {
         printf("   ");
     }
     printf("%d\n", root->value);
-    printTree(root->left, level + 1);
+    PrintTree(root->left, level + 1);
 }
 
 int main() {
@@ -76,13 +76,13 @@ int main() {
     char child;
     scanf("%d", &n);
     scanf("%d", &rootValue);
-    Node* root = createNode(rootValue);
+    Node* root = CreateNode(rootValue);
     for (int i = 0; i < n - 1; i++) {
         scanf("%d %d %c", &parentValue, &value, &child);
-        insert(&root, parentValue, value, child);
+        Insert(&root, parentValue, value, child);
     }
-    int maxDepth = getMaxDepth(root);
-    int maxValue = findMaxValue(root, maxDepth);
+    int maxDepth = GetMaxDepth(root);
+    int maxValue = FindMaxValue(root, maxDepth);
     if (maxValue == -1) {
         printf("Дерево пустое\n");
     } else if (maxValue == -2) {
@@ -91,6 +91,6 @@ int main() {
         printf("Значение вершины с максимальной глубиной: %d\n", maxValue);
     }
     printf("Дерево:\n");
-    printTree(root, 0);
+    PrintTree(root, 0);
     return 0;
 }
