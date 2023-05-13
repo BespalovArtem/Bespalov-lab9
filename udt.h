4532433c0d719acbb52fc8cfa91c0d9181308296
Bespalov Artem <75415627+BespalovArtem@users.noreply.github.com>
2023-05-13 12:01:07 +0300

    Update udt.h

--- udt.h
+++ udt.h
@@ -19,13 +19,13 @@ typedef struct _Udt
 	int _size;
 } Udt;
 
-void udtCreate(Udt *udt, const int capacity);
-int udtPush(Udt *udt, const UDT_TYPE value);
-void udtPop(Udt *udt);
-UDT_TYPE udtTop(const Udt *udt);
-int udtSize(const Udt *udt);
-int udtEmpty(const Udt *udt);
-void udtPrint(Udt *udt);
-void udtDestroy(Udt *udt);
+void UdtCreate(Udt *udt, const int capacity);
+int UdtPush(Udt *udt, const UDT_TYPE value);
+void UdtPop(Udt *udt);
+UDT_TYPE UdtTop(const Udt *udt);
+int UdtSize(const Udt *udt);
+int UdtEmpty(const Udt *udt);
+void UdtPrint(Udt *udt);
+void UdtDestroy(Udt *udt);
 
 #endif
